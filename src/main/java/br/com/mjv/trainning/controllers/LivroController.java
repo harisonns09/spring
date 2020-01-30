02c47af3e7330bbc37aaa2a24245f72a64ce16f3
Tiago Monnerat de Faria Lopes <tiago.lopes@MJV.net>
2020-01-30 16:48:38 -0300

    LivroController

--- src/main/java/br/com/mjv/trainning/controllers/LivroController.java
+++ src/main/java/br/com/mjv/trainning/controllers/LivroController.java
@@ -1,8 +1,23 @@
 package br.com.mjv.trainning.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.mjv.trainning.dao.livro.LivroDAO;
+
+@Controller
+@RequestMapping("/livros")
 public class LivroController {
 	
+	@Autowired
+	private LivroDAO dao;
 	
-	
+	@RequestMapping("/form")
+	public ModelAndView showForm() {
+		ModelAndView mv = new ModelAndView("livros/form");
+		return mv;
+	}
 
 }
