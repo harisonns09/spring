9495baf32ad0db9e79e74113c3ab98f7efdee04e
harisonns09 <harison.santos@MJV.net>
2020-01-30 11:38:29 -0300

    primeiro commit

---
+++ src/main/java/br/com/mjv/trainning/models/Livro.java
@@ -0,0 +1,48 @@
+package br.com.mjv.trainning.models;
+
+import java.math.BigDecimal;
+
+public class Livro {
+
+	private String titulo;
+	private String autor;
+	private Integer numeroPaginas;
+	private String editora;
+	private BigDecimal preco;
+	
+	
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public Integer getNumeroPaginas() {
+		return numeroPaginas;
+	}
+	public void setNumeroPaginas(Integer numeroPaginas) {
+		this.numeroPaginas = numeroPaginas;
+	}
+	public String getEditora() {
+		return editora;
+	}
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+	public BigDecimal getPreco() {
+		return preco;
+	}
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+	
+	
+	
+}
