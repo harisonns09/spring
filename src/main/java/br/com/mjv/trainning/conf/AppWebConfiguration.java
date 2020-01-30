c8c8d4345b3cd92a0175888cc3b7648b067f376d
Tiago Monnerat de Faria Lopes <tiago.lopes@MJV.net>
2020-01-30 16:31:05 -0300

    Limpando AppWeb e anotando Livros

--- src/main/java/br/com/mjv/trainning/conf/AppWebConfiguration.java
+++ src/main/java/br/com/mjv/trainning/conf/AppWebConfiguration.java
@@ -1,8 +1,5 @@
 package br.com.mjv.trainning.conf;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,15 +7,11 @@ import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
-import org.springframework.web.accept.ContentNegotiationManager;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -57,30 +50,9 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 		return new StandardServletMultipartResolver();
 	}
 
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
-
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
 	}
-
-	
-//	@Bean
-//	public ViewResolver contentNegotiationViewResolver(ContentNegotiationManager manager) {
-//
-//	    List<ViewResolver> viewResolvers = new ArrayList<ViewResolver>();
-//	    viewResolvers.add(internalResourceViewResolver());
-//	    viewResolvers.add(new JsonViewResolver());
-//
-//	    ContentNegotiatingViewResolver resolver = 
-//	        new ContentNegotiatingViewResolver();
-//	    resolver.setViewResolvers(viewResolvers);
-//	    resolver.setContentNegotiationManager(manager);
-//
-//	    return resolver;
-//	}
 	
 }
--- src/main/java/br/com/mjv/trainning/models/Livro.java
+++ src/main/java/br/com/mjv/trainning/models/Livro.java
@@ -1,63 +1,94 @@
 package br.com.mjv.trainning.models;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
-public class Livro {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-	private int id;
+@Entity
+public class Livro implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
 	private String titulo;
 	private String autor;
 	private Integer numeroPaginas;
 	private String editora;
 	private BigDecimal preco;
 	private String descricao;
-	
-	
-	
+
+	public Livro() {
+
+	}
+
+	public Livro(String titulo, String autor, Integer numeroPaginas) {
+		super();
+		this.titulo = titulo;
+		this.autor = autor;
+		this.numeroPaginas = numeroPaginas;
+	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getAutor() {
 		return autor;
 	}
+
 	public void setAutor(String autor) {
 		this.autor = autor;
 	}
+
 	public Integer getNumeroPaginas() {
 		return numeroPaginas;
 	}
+
 	public void setNumeroPaginas(Integer numeroPaginas) {
 		this.numeroPaginas = numeroPaginas;
 	}
+
 	public String getEditora() {
 		return editora;
 	}
+
 	public void setEditora(String editora) {
 		this.editora = editora;
 	}
+
 	public BigDecimal getPreco() {
 		return preco;
 	}
+
 	public void setPreco(BigDecimal preco) {
 		this.preco = preco;
 	}
-	
-	
-	
+
 }
